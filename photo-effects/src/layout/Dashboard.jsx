ceab9834aa4ccbb525c09c613c669f9ce5cffea7
Reyaad Rafeek <reyaad510@gmail.com>
2019-08-23 12:02:45 -0400

    created logout for dashboard(simple push to landing no credentials yet

--- photo-effects/src/layout/Dashboard.jsx
+++ photo-effects/src/layout/Dashboard.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Logout from '../components/Dashboard/Logout';
 
 export default class Dashboard extends Component {
+
+
+  logoutButton = e => {
+    e.preventDefault();
+    console.log(this.props)
+    this.props.history.push('/home');
+  }
+
   render() {
     return (
       <div>
-        Dashboard
+        <Logout logoutButton={this.logoutButton} />
+       <h1>Welcome Username!</h1>
+       
       </div>
     )
   }
