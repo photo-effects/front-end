635522a4e33d7d47cfc59dbbd2514f2586eede84
Edgar F <eflores.ef@gmail.com>
2019-08-22 19:29:08 -0700

    Deploy testing and display backend data.

---
+++ photo-effects/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Users from './components/Users';
+
+function App() {
+ 
+  return (
+    <div>
+      <h1>Photo Effects</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
