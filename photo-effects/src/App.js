cd5f2c2971c1e8431e81402b0fcb2f350e71947f
Christian-Ford <fjord.christian@gmail.com>
2019-08-23 21:02:05 -0500

    Created App class | constructor for Auth0

--- photo-effects/src/App.js
+++ photo-effects/src/App.js
@@ -3,15 +3,26 @@ import { Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Users from './components/Users';
+import Auth from './components/Auth';
 
-function App() {
- 
+class App extends Component {
+ // Constructor for Auth0
+ constructor(props) {
+   super(props);
+   this.auth = new Auth(this.props.history);
+ }
+
+
+ render() {
   return (
+    <>
     <div>
       <h1>Photo Effects</h1>
       <Users />
     </div>
+    </>
   );
+ }
 }
 
 export default App;
