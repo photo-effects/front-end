2d52725f970c55c7690aa60f3b28ef93df3f46a1
Ashley <jal9626@gmail.com>
2019-08-25 17:32:14 -0400

    added authentication

--- photo-effects/src/App.js
+++ photo-effects/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import Users from './components/Users';
-import Auth from './components/Auth';
-import Home from './components/Home'
+import Auth from './components/Auth/Auth';
+import Home from './components/Home';
+import Callback from './components/Callback';
+import Profile from './components/Profile';
 
 class App extends Component {
  // Constructor for Auth0
@@ -22,6 +23,13 @@ class App extends Component {
         path="/" exact 
         render = {props => <Home auth={this.auth} {...props} />} 
       />
+       <Route 
+        path="/callback"
+        render = {props => <Callback auth={this.auth} {...props} />} 
+      />
+      <Route
+       path="/profile" component={Profile}
+      /> 
     </div>
   );
  }
