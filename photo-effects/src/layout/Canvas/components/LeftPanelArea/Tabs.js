4cf032cf59cb21167ccd44bf59d3ab69390d777a
Edgar F <eflores.ef@gmail.com>
2019-09-04 19:32:41 -0700

    Resolved

--- photo-effects/src/layout/Canvas/components/LeftPanelArea/Tabs.js
+++ photo-effects/src/layout/Canvas/components/LeftPanelArea/Tabs.js
@@ -5,30 +5,15 @@ function Tabs(props) {
     return (
       <div style={tabContainer}>
         <div style={tabList}>
-<<<<<<< HEAD
           {props.children.map(child => {
-=======
-          {this.props.children.map((child, i) => {
-
-            const tabContent = child.props.children;
-            
->>>>>>> master
             return (
-              <React.Fragment
-                key = { i }
-              >
+              <>
                  <Tab
                    key={child.props.label}   
                    tabName={child.props.label}
                    tabContent={child.props.children}
                  />
-<<<<<<< HEAD
               </>
-=======
-
-                 {child.props.label === this.state.activeTab ? tabContent : undefined}
-              </React.Fragment>
->>>>>>> master
             );
           })}
         </div>
