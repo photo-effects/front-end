8a7382e8fff69b90b3f0c49de88ee3766814452e
Christian-Ford <fjord.christian@gmail.com>
2019-09-08 18:04:43 -0500

    Styled the landing page to be more erganomic

--- photo-effects/src/components/Landing/nav/Navbar.jsx
+++ photo-effects/src/components/Landing/nav/Navbar.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import Photolist from '../../waterfall/Photolist';
 import TagsCont from '../../Landing/Tags/TagsCont';
 import './nav.css';
+
+import navLogo from '../../../assetts/photoLogo.png';
 export class Navbar extends Component {
 //push to landing
   render() {
     return (
       <nav>
       <div>
-        <h1 className="logo">Photo Effects</h1>
+        <img src= {navLogo} alt="navbar logo" className="navbarLogo" />
+        {/* <h1 className="logo">Photo Effects</h1> */}
       </div>
       {/* <p className="navlinks loginbutton">
         <i className="far fa-plus-square plus" /> New Project
