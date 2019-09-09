f54a2013f2b8e1ce79affba9c60e92f41de0c052
Christian-Ford <fjord.christian@gmail.com>
2019-09-08 21:55:40 -0500

    Passed props from slider into toolbar

--- photo-effects/src/layout/TextBox/Slider.js
+++ photo-effects/src/layout/TextBox/Slider.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 import InputRange from 'react-input-range';
 
-import 'react-input-range/lib/css/index.css'
+import 'react-input-range/lib/css/index.css';
+
+import './SliderStyle.css'
 
 export class Slider extends Component {
     constructor(props) {
@@ -22,6 +24,7 @@ export class Slider extends Component {
         return (
             <div style={{marginTop: '30px', height: '50px', width: '200px',display: 'flex', flexDirection: 'column', }}>
                 <InputRange
+                    style= {{backgroundColor: 'red'}}
                     maxValue={180}
                     minValue={0}
                     value={this.state.value}
