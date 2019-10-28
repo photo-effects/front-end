f424ab073300b61edd7e676634c09c1bcdbc4fc5
Christian-Ford <fjord.christian@gmail.com>
2019-10-28 10:53:01 -0500

    Initial work on About Page

--- photo-effects/src/Pages/About/About.jsx
+++ photo-effects/src/Pages/About/About.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import Navbar from '../Landing/components/nav/Navbar';
 
+const divColor = {
+  display: 'flex' ,
+  flexDirection: 'column',
+  justifyContent: 'center' ,
+  alignItems: 'center',
+  marginTop: '80px',
+  fontSize: '36px'
+}
+
+const center = {
+  marginTop: '10px' ,
+  display: 'flex' ,
+  flexDirection: 'column',
+  justifyContent: 'center' ,
+  alignItems: 'center' ,
+  fontSize: '24px',
+  textAlign: 'center',
+  border: '1px solid black',
+  
+}
+
 const About = (props) => {
   return (
-    <div>
+    <>
       <Navbar auth={props.auth} />
+      
       <div>
-      <h1>About</h1>
+        <h1>This has to be here</h1>
+        <h2 style={divColor}> About The Team</h2>
+        <h3 style={center}> 
+          Photo Effects was created by a Team from Lambda School
+          with the intentions on creating a full scale web application
+          within the likes of real companies. It was built over a 4
+          month time frame, worked on part time by the team.
+          
+        </h3>
       </div>
-    </div>
+    </>
   )
 }
 
