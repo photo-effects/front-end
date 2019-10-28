deffea2eb65d6f6440201f2f90906f12f049e75c
Christian-Ford <fjord.christian@gmail.com>
2019-10-28 17:03:40 -0500

    Finished most of about page

--- photo-effects/src/Pages/About/About.jsx
+++ photo-effects/src/Pages/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Landing/components/nav/Navbar';
 import Footer from '../Landing/components/Footer/Footer';
+import Pic from  '../../assetts/meAndGuitar.jpg';
 
 const divColor = {
   margin: '0 auto' ,
@@ -25,7 +26,7 @@ const divColor2 = {
 }
 
 const aboutSection = {
-  marginTop: '30px' ,
+  marginTop: '10px' ,
   padding: '25px' ,
   fontSize: '24px',
   textAlign: 'center',
@@ -37,12 +38,56 @@ const aboutSection = {
 
 }
 
+const teamSection = {
+  marginTop: '10px' ,
+  padding: '25px' ,
+  fontSize: '24px',
+  textAlign: 'center',
+  border: '1px solid black',
+  backgroundColor: '#364F6B' ,
+  color: 'white',
+  fontWeight: 'none',
+  width: '80%'
+}
+
+const listWrap = {
+  display: 'flex' ,
+  flexDirection: 'row',
+  justifyContent: 'center',
+  marginTop: '10px'
+}
+
+const listItem = {
+  border: '1px solid yellow',
+  height: '300px' ,
+  width: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '5px',
+  backgroundColor: 'black'
+}
+
 const bodyColor = {
   backgroundColor: '#D0D0D0'
 }
 
 const footerExtra = {
   marginBottom : '25px '
+}
+
+const profilePic = {
+  height: '150px',
+  width: '150px',
+  marginTop: '50px'
+}
+
+const buttonStyle = {
+  marginTop: '10px',
+  width: '70px',
+  height: '30px',
+  border: '1px solid grey',
+  backgroundColor: '#d0d0d0'
 }
 
 
@@ -55,8 +100,8 @@ const About = (props) => {
         <h1>This has to be here</h1>
 
         <div style={divColor}>
-          <h2 style={{color:'white'}}> About The Team</h2>
-          <p style={aboutSection}> 
+          <h2 style={{color:'black'}}> About The Team</h2>
+          <p style={teamSection}> 
             Photo Effects was created by a Team from Lambda School
             with the intentions on creating a full scale web application
             within the likes of real companies. It was built over a 4
@@ -68,13 +113,39 @@ const About = (props) => {
         <div style={divColor}>
           <p style={aboutSection}> 
             The team behind Photo Effects :
-            <br />
-            <br /> Christian Ford
-            <br /> Jasmine Logan
-            <br /> Douglas Jordan
-            <br /> Nicholas Rafeek
-            <br /> Edgar Flores
-            <br /> Joe Bugajski
+            <ul style={listWrap}>
+              <li style={listItem}>Christian Ford 
+              <img style={profilePic} src={Pic} />
+              <button style={buttonStyle}>GitHub</button>
+              </li>
+
+              <li style={listItem}>Jasmine Logan
+              <img style={profilePic} src={Pic} />
+              <button style={buttonStyle}>GitHub</button>
+              </li>
+
+              <li style={listItem}>Douglas Jordan
+              <img style={profilePic} src={Pic} />
+              <button style={buttonStyle}>GitHub</button>
+              </li>
+            </ul>
+
+            <ul style={listWrap}>
+              <li style={listItem}>Edgar Flores
+              <img style={profilePic} src={Pic} />
+              <button style={buttonStyle}>GitHub</button>
+              </li>
+
+              <li style={listItem}>Nicholas Rafeek
+              <img style={profilePic} src={Pic} />
+              <button style={buttonStyle}>GitHub</button>
+              </li>
+
+              <li style={listItem}>Joe Bugajksi
+              <img style={profilePic} src={Pic} />
+              <button style={buttonStyle}>GitHub</button>
+              </li>
+            </ul>
            </p>
         </div>
 
