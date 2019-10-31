4a0379e6c08f78c050ad980692ae6bad613b4b48
claridiva2000 <claridiva2000@gmail.com>
2019-10-30 20:06:16 -0500

    docs pg bootstraped

--- photo-effects/src/Pages/Docs/Docs.jsx
+++ photo-effects/src/Pages/Docs/Docs.jsx
@@ -1,50 +1,75 @@
-import React from "react";
-import Navbar from "../Landing/components/nav/Navbar";
+import React from 'react';
+import Navbar from '../Landing/components/nav/Navbar';
 // import { HashLink as Link } from "react-router-hash-link";
 
 const Docs = ({ auth }) => {
+  
+  const h3s = {
+    marginTop:'10px'
+  }
+
+  const sections = {
+    marginTop:'15px'
+  }
+  
   const menu = {
-    minWidth: "180px",
-    padding: "10px",
-    height: "1000px",
-    overflow: "auto",
-    background: "#FC5185",
-    color: "#E5E5E6",
-    fontSize: "16px",
-    fontWeight: "bold"
+    minWidth: '180px',
+    padding: '16px',
+    height: '850px',
+    overflow: 'auto',
+    background: '#FC5185',
+    color: '#E5E5E6',
+    fontSize: '16px',
+    fontWeight: 'bold',
   };
 
   const docsMenu = {
-    width: "100%",
-    fontSize: "16px",
-    margin: "5px"
+    width: '100%',
+    height: '850px',
+    fontSize: '16px',
+    margin: '5px',
+    overflow:'auto'
   };
 
-  const listItems = [{ id: 1, name: "First Item", link: "link stuff" }];
+  const listItems = [
+    { id: 1, name: 'First Item', link: 'link stuff' },
+    { id: 2, name: '2nd Item', link: 'link stuff' },
+    { id: 3, name: '3rd Item', link: 'link stuff' },
+    { id: 4, name: '4th Item', link: 'link stuff' },
+    { id: 5, name: '5th Item', link: 'link stuff' },
+    { id: 6, name: '6th Item', link: 'link stuff' },
+    { id: 7, name: '7th Item', link: 'link stuff' },
+    { id: 8, name: '8th Item', link: 'link stuff' },
+    { id: 9, name: '9th Item', link: 'link stuff' },
+    { id: 10, name: '10th Item', link: 'link stuff' }
+  ];
 
   const scrollFunc = id => {
+    // let scrollId = id + 1;
+    console.log(id);
+
     let element = document.getElementById(id);
-    element.scrollIntoView();
+    element.scrollIntoView({behavior: "smooth"});
   };
 
   return (
-    <div>
+    <div style={{height:'945px', overflow:'hidden'}}>
       <Navbar auth={auth} />
-      <div style={{ height: "200px" }}></div>
+      <div style={{ height: '100px' }}></div>
 
-      <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
         <div style={menu}>
           <ul>
             {listItems.map(item => (
-              <li onClick={() => scrollFunc(item.id)}>
+              <li style={{marginTop:'10px'}} onClick={() => scrollFunc(item.id)}>
                 {item.id}. {item.name}
               </li>
             ))}
           </ul>
         </div>
         <div style={docsMenu}>
-          <div>
-            <h3>First thing first</h3>
+          <div id="1" style={sections}>
+            <h3 style={h3s}>1st thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
             ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
@@ -56,8 +81,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <div>
-            <h3 id="1">First thing first</h3>
+          <div id="2"  style={sections}>
+            <h3 style={h3s}>2nd thing </h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
             ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
@@ -69,8 +94,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <div>
-            <h3>First thing first</h3>
+          <div id="3"  style={sections}>
+            <h3 style={h3s}>3rd thing </h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
             ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
@@ -82,8 +107,164 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <div>
-            <h3>First thing first</h3>
+          <div id="4"  style={sections}>
+            <h3 style={h3s}>4th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="5"  style={sections}>
+            <h3>5th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="6"  style={sections}>
+            <h3>6th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="7"  style={sections}>
+            <h3>7th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="8"  style={sections}>
+            <h3>8th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="9"  style={sections}>
+            <h3>9th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="10"  style={sections}>
+            <h3>10th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="11"  style={sections}>
+            <h3>11th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="12"  style={sections}>
+            <h3>12th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="13"  style={sections}>
+            <h3>13th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="14"  style={sections}>
+            <h3>14th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="15" style={sections} >
+            <h3>15th thing</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+            sed pariatur illum facilis modi neque, esse repellat iste ipsa
+            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
+            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
+            odio aliquam nihil sint explicabo tempora minus ratione tempore
+            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
+            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
+            vitae labore enim natus quasi laborum culpa mollitia?
+          </div>
+          <div id="16"  style={sections}>
+            <h3>16th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
             ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
