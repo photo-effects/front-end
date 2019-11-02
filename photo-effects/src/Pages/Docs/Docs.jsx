d783e9993ad5389c34a024c7f63ef013c984d661
Reyaad Rafeek <reyaad510@gmail.com>
2019-11-02 13:58:02 -0400

    Added dashboard to docs and it's components

--- photo-effects/src/Pages/Docs/Docs.jsx
+++ photo-effects/src/Pages/Docs/Docs.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
-import Navbar from '../Landing/components/nav/Navbar';
+import React from "react";
+import Navbar from "../Landing/components/nav/Navbar";
+import dashState from "../../assetts/dashboardState.png";
 // import { HashLink as Link } from "react-router-hash-link";
 
 const Docs = ({ auth }) => {
   const h3s = {
-    fontSize: '24px',
-    marginTop: '10px'
+    fontSize: "24px",
+    marginTop: "10px"
   };
 
   const sections = {
-    marginTop: '15px'
+    marginTop: "15px"
   };
 
   const menu = {
-    minWidth: '180px',
-    padding: '16px',
-    height: '850px',
-    overflow: 'auto',
-    background: '#FC5185',
-    color: '#E5E5E6',
-    fontSize: '16px',
-    fontWeight: 'bold'
+    minWidth: "180px",
+    padding: "16px",
+    height: "850px",
+    overflow: "auto",
+    background: "#FC5185",
+    color: "#E5E5E6",
+    fontSize: "16px",
+    fontWeight: "bold"
   };
 
   const docsMenu = {
-    width: '100%',
-    height: '850px',
-    fontSize: '16px',
-    margin: '5px',
-    overflow: 'auto'
+    width: "100%",
+    height: "850px",
+    fontSize: "16px",
+    margin: "5px",
+    overflow: "auto"
   };
 
   const listItems = [
-    { id: 1, name: 'Tech Stack', link: 'link stuff' },
-    { id: 2, name: 'Dependencies', link: 'link stuff' },
-    { id: 3, name: 'Overview', link: 'link stuff' },
-    { id: 4, name: 'Links, Docs and more', link: 'link stuff' },
-    { id: 5, name: '5th Item', link: 'link stuff' },
-    { id: 6, name: '6th Item', link: 'link stuff' },
-    { id: 7, name: '7th Item', link: 'link stuff' },
-    { id: 8, name: '8th Item', link: 'link stuff' },
-    { id: 9, name: '9th Item', link: 'link stuff' },
-    { id: 10, name: '10th Item', link: 'link stuff' }
+    { id: 1, name: "Tech Stack", link: "link stuff" },
+    { id: 2, name: "Dependencies", link: "link stuff" },
+    { id: 3, name: "Overview", link: "link stuff" },
+    { id: 4, name: "Links, Docs and more", link: "link stuff" },
+    { id: 5, name: "5th Item", link: "link stuff" },
+    { id: 6, name: "Dashboard", link: "link stuff" },
+    { id: 7, name: "7th Item", link: "link stuff" },
+    { id: 8, name: "8th Item", link: "link stuff" },
+    { id: 9, name: "9th Item", link: "link stuff" },
+    { id: 10, name: "10th Item", link: "link stuff" }
   ];
 
   const scrollFunc = id => {
@@ -49,20 +50,20 @@ const Docs = ({ auth }) => {
     console.log(id);
 
     let element = document.getElementById(id);
-    element.scrollIntoView({ behavior: 'smooth' });
+    element.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
-    <div style={{ height: '937px', overflow: 'hidden' }}>
+    <div style={{ height: "937px", overflow: "hidden" }}>
       <Navbar auth={auth} />
-      <div style={{ height: '105px' }}></div>
+      <div style={{ height: "105px" }}></div>
 
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
         <div style={menu}>
           <ul>
             {listItems.map(item => (
               <li
-                style={{ marginTop: '10px' }}
+                style={{ marginTop: "10px" }}
                 onClick={() => scrollFunc(item.id)}
               >
                 {item.id}. {item.name}
@@ -169,7 +170,7 @@ const Docs = ({ auth }) => {
                 </a>
               </li>
               <li>
-                {' '}
+                {" "}
                 <a
                   href="https://www.npmjs.com/package/auth0-js"
                   target="_blank"
@@ -256,7 +257,7 @@ const Docs = ({ auth }) => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  {' '}
+                  {" "}
                   react-particles-js
                 </a>
               </li>
@@ -334,7 +335,6 @@ const Docs = ({ auth }) => {
                 </a>
               </li>
             </ul>
-            
           </div>
           <span id="5"></span>
           <div style={sections}>
@@ -352,19 +352,77 @@ const Docs = ({ auth }) => {
           </div>
           <span id="6"></span>
           <div style={sections}>
-            <h3>6th thing</h3>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
-            sed pariatur illum facilis modi neque, esse repellat iste ipsa
-            ipsum, fuga nobis! Vitae exercitationem incidunt impedit doloribus
-            distinctio! Delectus, aliquid. Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Cumque odit excepturi fuga fugit quod,
-            odio aliquam nihil sint explicabo tempora minus ratione tempore
-            laborum nam repellendus esse iusto maiores alias. Lorem ipsum dolor,
-            sit amet consectetur adipisicing elit. Accusantium tenetur ab autem
-            eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
-            vitae labore enim natus quasi laborum culpa mollitia?
+            <h3>Dashboard</h3>
+            <p>To find the Dashboard component go into Pages --> Dashboard.</p>
+            <div>
+              <img
+                style={{ width: "300px", height: "200px" }}
+                src={dashState}
+              />
+            </div>
+            <p>This is the state in Dashboard</p>
+            <p> users: Obtaining users from our backend</p>
+            <p>
+              images: This is where the image from cloudinary is stored
+              initially when the user chooses an image from their computer to
+              use via dragndrop or choosing file.
+            </p>
+            <p>
+              uploading: Used when user chooses a photo to tell them the photo
+              is waiting to load to be displayed from Cloudinary
+            </p>
+            <p>
+              error: Error will be displayed if user chooses image that is too
+              big or if trying to upload multiple images at once
+            </p>
+            <p>
+              inputKey: Needed to reset the "Choose File" option if user decides
+              to choose a differnt photo.(It's a little hack)
+            </p>
+            <p>exist: Used for conditional rendering</p>
+            <p>canvasprojects: Pulling projects from our backend</p>
+            <div>
+              <h3>Methods:</h3>
+              <p>
+                onChange: There are two of these in Dashboard.jsx. Two is needed
+                because one is used for when a user clicks "Choose File" and the
+                second is used if the user uploads a photo via DragNDrop.
+              </p>
+            </div>
+            <div>
+              <p>
+                {" "}
+                Dashboard has 4 components which are DashNav, Image, Projects,
+                and Upload
+              </p>
+              <p>DashNav is simply the component that deals with the navbar</p>
+              <p>
+                Image component also holds a component called PhotoLink. These
+                components deal with the image that is being displayed ot the
+                user. The image is being displayed by returning a "secure_url"
+                from Cloudinary. The image has all of the data coming back from
+                Cloudinary in Dashboard.jsx state mentioned above. In PhotoLink
+                is where the user can click "Go to canvas" or to "Choose a
+                different Image". Going to canvas will push the data to our
+                backend(see the info pushed in PhotoLink component) and "Choose
+                a different image" will delete the image from Cloudinary and
+                allow the user to choose another image
+              </p>
+              <p>
+                Projects is the component that displays the specific user's
+                projects on the bottom of the Dashboard page. It is being mapped
+                from canvasprojects from the state in Dashboard.jsx. A user can
+                click on a photo to go to the canvas with the image displayed
+                and edit the image{" "}
+              </p>
+              <p>
+                Upload component has the react-dragndrop code for when a user
+                wants to choose a file to upload
+              </p>
+            </div>
           </div>
-          <span id="7" ></span>
+
+          <span id="7"></span>
           <div style={sections}>
             <h3>7th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
@@ -378,8 +436,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span id="8" ></span>
-          <div  style={sections}>
+          <span id="8"></span>
+          <div style={sections}>
             <h3>8th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
@@ -392,8 +450,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span id="9" ></span>
-          <div  style={sections}>
+          <span id="9"></span>
+          <div style={sections}>
             <h3>9th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
@@ -406,8 +464,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span id="10" ></span>
-          <div  style={sections}>
+          <span id="10"></span>
+          <div style={sections}>
             <h3>10th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
@@ -420,8 +478,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span id="11" ></span>
-          <div  style={sections}>
+          <span id="11"></span>
+          <div style={sections}>
             <h3>11th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
@@ -434,8 +492,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span id="12" ></span>
-          <div  style={sections}>
+          <span id="12"></span>
+          <div style={sections}>
             <h3>12th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
@@ -448,8 +506,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span id="13" ></span>
-          <div  style={sections}>
+          <span id="13"></span>
+          <div style={sections}>
             <h3>13th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
@@ -462,8 +520,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span id="14" ></span>
-          <div  style={sections}>
+          <span id="14"></span>
+          <div style={sections}>
             <h3>14th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
@@ -476,7 +534,7 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span  id="15" ></span>
+          <span id="15"></span>
           <div style={sections}>
             <h3>15th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
@@ -490,8 +548,8 @@ const Docs = ({ auth }) => {
             eius? Non blanditiis unde ipsum expedita in explicabo commodi. Dolor
             vitae labore enim natus quasi laborum culpa mollitia?
           </div>
-          <span  id="16" ></span>
-          <div  style={sections}>
+          <span id="16"></span>
+          <div style={sections}>
             <h3>16th thing</h3>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
             sed pariatur illum facilis modi neque, esse repellat iste ipsa
